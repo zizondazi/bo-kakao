9b2abbf75d7e3b9f2aad55483e898179ef246ab6
KDS <ektmf0129@gmail.com>
2022-11-08 16:52:47 +0900

    시작 시 크롤링 실행 여부 추가

--- src/main/java/com/bokakao/config/StartConfig.java
+++ src/main/java/com/bokakao/config/StartConfig.java
@@ -1,5 +1,6 @@
 package com.bokakao.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,12 +12,14 @@ public class StartConfig implements CommandLineRunner {
 	
 	@Value("${isExecutorCrawling}")
 	private boolean isExecutorCrawling;
+	
+	@Autowired
+	CrawlingBatchExecutor crawling;
 
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("isExecutorCrawling :: " + isExecutorCrawling);
 		if(isExecutorCrawling) {
-			CrawlingBatchExecutor crawling = new CrawlingBatchExecutor();
 			crawling.crawling();
 		}
 	}
