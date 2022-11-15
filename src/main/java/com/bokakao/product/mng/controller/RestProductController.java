da54b2e25d07fe772ee0c86b563a8f8ff581074b
KDS <ektmf0129@gmail.com>
2022-11-15 13:54:24 +0900

    제품 목록 매개변수 추가

--- src/main/java/com/bokakao/product/mng/controller/RestProductController.java
+++ src/main/java/com/bokakao/product/mng/controller/RestProductController.java
@@ -11,15 +11,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.beust.jcommander.Parameter;
 import com.bokakao.product.mng.domain.ProductMngDomain;
 import com.bokakao.product.mng.service.ProductMngService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @Api (tags = {"RestProductController Api v1 - 제품"})
 @RestController
@@ -31,17 +34,20 @@ public class RestProductController {
 
 	@ApiOperation(value = "제품 목록 전체 조회",  notes = "<strong style='color:red;'>제품</strong> 목록을 조회한다.")
 	@GetMapping()
-	public ResponseEntity<?> getProductList(HttpServletRequest request) {
+	public ResponseEntity<?> getProductList(
+			@ApiParam(value = "페이지 번호", required = false) @RequestParam(required = false) Integer page_no,
+			@ApiParam(value = "페이지 사이즈", required = false) @RequestParam(required = false) Integer page_size
+			) {
 		List<ProductMngDomain> prdt_list = new ArrayList<ProductMngDomain>();
 		ProductMngDomain prdt = new ProductMngDomain();
 		
 		try {
-			prdt.setPage_no(Integer.parseInt(request.getParameter("page_no")));
-			prdt.setPage_size(Integer.parseInt(request.getParameter("paze_size")));
+			if(page_no != null) prdt.setPage_no(page_no);
+			if(page_size != null) prdt.setPage_size(page_size);
 			
 			prdt_list = productMngService.getProductList(prdt);
 		}catch (Exception e) {
-			
+			e.printStackTrace();
 		}
 		return new ResponseEntity<>(prdt_list, HttpStatus.OK);
 	}
